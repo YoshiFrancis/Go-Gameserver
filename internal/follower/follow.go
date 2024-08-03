a0d88c20d5ddd5ea1e2fdbe38ff431f9f0dbd48c
YoshiFrancis <francis.ozua@gmail.com>
2024-08-03 00:46:08 -0500

    added template folder. YES. I CAN SUCCESFULLY PROMPT FOR USERNAME AND STORE IN WS. LET's GO

--- internal/follower/follow.go
+++ internal/follower/follow.go
@@ -29,7 +29,6 @@ func Follower_init(wsPort, leaderIp string) {
 	// go tcp.Run()
 	go ws.Run()
 
-	http.HandleFunc("/username", ws.Username)
 	http.HandleFunc("/home", ws.Home)
 	http.HandleFunc("/", ws.Index)
 
--- internal/follower/wsserver/wsserver.go
+++ internal/follower/wsserver/wsserver.go
@@ -1,6 +1,7 @@
 package wsserver
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -16,8 +17,8 @@ var indexTemplate *template.Template
 var usernameTemplate *template.Template
 
 func init() {
-	indexTemplate = template.Must(template.ParseFiles("../internal/follower/wsserver/index.html"))
-	usernameTemplate = template.Must(template.ParseFiles("../internal/follower/wsserver/username.html"))
+	indexTemplate = template.Must(template.ParseFiles("../web/index.html"))
+	usernameTemplate = template.Must(template.ParseFiles("../internal/templates/username.html"))
 }
 
 var upgrader = websocket.Upgrader{
@@ -57,15 +58,18 @@ type Username struct {
 }
 
 func (ws *WSServer) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("serving home")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Upgrade error: ", err)
 		return
 	}
 
+	promptUsernameTemp := renderTemplate(struct{}{})
+	conn.WriteMessage(websocket.TextMessage, promptUsernameTemp)
+
 	// Read message from browser
 	_, p, err := conn.ReadMessage()
+
 	if err != nil {
 		log.Println("error reading: ", err)
 		return
@@ -81,35 +85,7 @@ func (ws *WSServer) Home(w http.ResponseWriter, r *http.Request) {
 	NewClient(username.Username, conn, ws)
 	register_msg := messages.ServerJoinUser(username.Username, -1)
 	ws.TCPto <- []byte(register_msg)
-}
 
-func (ws *WSServer) Username(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Unable to parse form", http.StatusBadRequest)
-			return
-		}
-
-		// Read the input value
-		username := r.FormValue("username")
-		if username == "" {
-			http.Error(w, "Username is required", http.StatusBadRequest)
-			return
-		}
-
-		err = indexTemplate.Execute(w, struct {
-			Username string
-		}{
-			Username: username,
-		},
-		)
-		if err != nil {
-			http.Error(w, "Failed to render template", http.StatusInternalServerError)
-			fmt.Println("Erorr rendering index template: ", err)
-			return
-		}
-	}
 }
 
 func (ws *WSServer) Run() {
@@ -136,10 +112,16 @@ func (ws *WSServer) Shutdown() {
 }
 
 func (ws *WSServer) Index(w http.ResponseWriter, r *http.Request) {
-	err := usernameTemplate.Execute(w, nil)
+	err := indexTemplate.Execute(w, nil)
 	if err != nil {
 		http.Error(w, "Failed to render template", http.StatusInternalServerError)
 		fmt.Println("Error rendering username template:", err)
 		return
 	}
+}
+
+func renderTemplate[T any](dataStruct T) []byte {
+	var templateBuffer bytes.Buffer
+	usernameTemplate.Execute(&templateBuffer, dataStruct)
+	return templateBuffer.Bytes()
 }
